ff69ce532fbcfc7341161199b74c928449429d84
Ryan-Geiser <rgeiser1@gmail.com>
2024-02-14 17:43:18 -0800

    trying to trouble shoot #about me

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,16 +1,19 @@
-var tabLinks = document.getElementById('tab-links');
-var tabContents = document.getElementById('tab-contents');
+var tabLinks = document.getElementById("tab-links");
 
-function openTab(tabName) {
-    for(tabLinks of tabLinks) {
-        tabLinks.classList.remove('active-link');
-    }
-    for(tabContents of tabContents) {
-        tabContents.classList.remove('active-tab');
-    }
+function openTab(tabname) {
+    var tabLinks = document.querySelectorAll(".tab-links");
+    var tabContents = document.querySelectorAll(".tab-contents");
+
+    tabLinks.forEach(function(link) {
+        link.classList.remove('active-link');
+    });
+
+    tabContents.forEach(function(content) {
+        content.classList.remove('active-tab');
+    });
+
     event.currentTarget.classList.add('active-link');
-    document.getElementById(tabName).classList.add('active-tab');
-
+    document.getElementById(tabname).classList.add("active-tab");
 }
 
 var sidebarMenu = document.getElementById("sidebar-menu");
